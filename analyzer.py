343d8234aba54c6592ff4f11672d04f8bf58cd4d
Lulu Roth <ls80zyse@studserv.uni-leipzig.de>
2019-06-11 12:02:29 +0200

    Refactor analyzer

--- analyzer.py
+++ analyzer.py
@@ -29,25 +29,38 @@ def get_log_file_names(path_to_log_dir: str) -> List[str]:
 
 def analyze_log_list(log_list: List[Dict]):
     list_length = len(log_list)
-    std_dev = compute_std_deviation(log_list)
-    logger.print_info(log_list[0].get(constants.REPORT).get(constants.TEST_NAME), std_dev)
+    std_dev = 0
+    if list_length >= 2:
+        std_dev = compute_std_deviation(log_list)
 
-    # compare perf of commit X with performance of following commit X+1 (an earlier versions)
-    logger.print_header()
+    analysis_result = {}
+    analysis_result['test_name'] = log_list[0].get(constants.REPORT).get(constants.TEST_NAME)
+    analysis_result['std_dev'] = std_dev
+    analysis_result['delta_threshold'] = DELTA_THRESHOLD
+    analysis_result['std_dev_threshold'] = STD_DEV_THRESHOLD
+    analysis_result['commits'] = []
+
+    # compare perf of commit X with performance of following commit X+1 (an earlier version)
     for i in range(list_length - 1):
         current_commit = log_list[i][constants.COMMIT]
-        next_commit = log_list[i + 1][constants.COMMIT]
         current_runtime = log_list[i][constants.REPORT][constants.TIME_ELAPSED]
         next_runtime = log_list[i + 1][constants.REPORT][constants.TIME_ELAPSED]
 
         runtime_delta = current_runtime - next_runtime
         speedup = current_runtime / next_runtime if int(next_runtime) is not 0 else 0
-        logger.log_delta(current_commit, current_runtime, runtime_delta, speedup)
-        if (current_runtime * DELTA_THRESHOLD) > next_runtime:
-            logger.warn(current_commit, next_commit, runtime_delta, DELTA_THRESHOLD)
+
+        commit_statistics = {}
+        commit_statistics['hexsha'] = current_commit
+        commit_statistics['runtime'] = current_runtime
+        commit_statistics['speedup'] = speedup
+        commit_statistics['runtime_delta'] = runtime_delta
+
+        analysis_result['commits'].append(commit_statistics)
+
+    logger.print_statistics(analysis_result)
 
 
-def compute_std_deviation(log_list) -> float:
+def compute_std_deviation(log_list: List[Dict]) -> float:
     runtimes = [log[constants.REPORT][constants.TIME_ELAPSED] for log in log_list]
     return statistics.stdev(runtimes)
 
--- logger.py
+++ logger.py
@@ -1,4 +1,5 @@
 import sys
+from typing import Dict
 
 def warn(current_commit: str, next_commit: str, delta: float, threshold: float):
     print("WARNING: {} introduced performance change. Delta: {}. Threshold: {}.".format(current_commit, delta, threshold), file=sys.stderr)
@@ -13,3 +14,15 @@ def print_header():
 
 def log_delta(current_commit: str, runtime: float, delta: float, speedup: float):
     print("{:<42}{:<9.3f}{:<9.3f}{:<5.3f}".format(current_commit, runtime, delta, speedup))
+
+
+def print_statistics(statistics: Dict):
+    print("\n{s[test_name]}"
+        "\n\nStd deviation: {s[std_dev]}"
+        "\nDelta threshold: {s[delta_threshold]}"
+        "\nStd deviation threshold: {s[std_dev_threshold]}\n".format(s=statistics))
+    
+    print_header()
+    
+    for commit_statistics in statistics['commits']:
+        print("{c[hexsha]:<42}{c[runtime]:<9.3f}{c[runtime_delta]:<9.3f}{c[speedup]:<5.3f}".format(c=commit_statistics))
