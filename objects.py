3a6f743db9ff65d9e64dcd922bbe2cafdacf2eeb
Schons <schons@studserv.uni-leipzig.de>
2019-08-05 18:36:08 +0200

    Add data structs PrimaryMetric & JmhReport

--- objects.py
+++ objects.py
@@ -37,6 +37,31 @@ class BenchmarkStatistics(NamedTuple):
     commits: List[CommitStatistics]
 
 
+class PrimaryMetric(NamedTuple):
+    """Represents the primary metrics obtained by a jmh benchmark"""
+    score: float
+    scoreError: float
+    scoreConfidence: List[float]
+    scoreUnit: str
+    rawData: List[List[float]]
+
+
+class JmhReport(NamedTuple):
+    """Data structure that represent data obtained by a running a jmh benchmark"""
+    benchmark: str
+    mode: str
+    thread: int
+    forks: int
+    jvm: str
+    jvmArgs: List[str]
+    jdkVersion: str
+    warmupIterations: int
+    warmupTime: str
+    measurementIterations: int
+    measurementTime: str
+    PrimaryMetric: PrimaryMetric
+
+
 def build_commit_report(report_data: Dict[str, Any]) -> CommitReport:
     """Takes a dict with CommitReport fields as keys and returns the CommitReport equivalent"""
     report = JUnitReport(**report_data[const.REPORT])
